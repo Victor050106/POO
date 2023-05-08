da8abb59d25df0c478a694ea39613aa868c7fe99
Victor <vimatoca050106@gmail.com>
2023-05-08 15:11:42 -0500

    se resumió más el codigo

--- Animal.py
+++ Animal.py
@@ -5,66 +5,37 @@ class Animal:
         self.genero = genero
         self.edad = edad
 
-    def eat(self):
+    def information(self):
         if self.nombre =="copito":
-            print(f"{self.nombre} se toma su leche favorita")
+            print(f"\n Nombre:{self.nombre} \n Comida favorita: Pescado \n Especie:{self.especie} \n Genero:{self.genero} \n Edad:{self.edad}")
         else:
-            print(f"{self.nombre} se come sus huesos favoritos que le trajo su dueño")
+            print(f"\n Nombre:{self.nombre} \n Comida favorita: Carne \n Especie:{self.especie} \n Genero:{self.genero} \n Edad:{self.edad}") 
 
-    def sleep(self):
-        if self.nombre =="copito":
-            print(f"{self.nombre} duerme mucho porque ya tiene {self.edad} años")
-        else:
-            print(f"{self.nombre} es mas activa y juguetona ya que tiene {self.edad} años")
+Animal1 = Animal("copito", "gato", "macho", " 7 años" )
+Animal2 = Animal("princesa", "perro", "hembra"," 5 años" )
 
-    def play(self):
-        if self.nombre == "princesa":
-            print(f"{self.nombre} juega mucho con su mejor amigo copito ")
-        else:
-            print(f"{self.nombre} le gusta jugar mucho con su mejor amiga princesa")
+Animal1.information()
+Animal2.information()
 
-    def especies(self):
-        if self.especie == "perro":
-            print(f"{self.nombre} es más brusca ya que es un {self.especie}")
-        else:
-            print(f"{self.nombre} es más cariñoso ya que es un {self.especie}")
-    
-    
-
-Animal1 = Animal("copito", "gato", "macho",  7 )
-Animal2 = Animal("princesa", "perro", "hembra", 5 )
-
-
-Animal1.eat()
-print("")
-Animal1.sleep()
-print("")
-Animal1.play()
-print("")
-Animal1.especies()
-print("")
-Animal2.eat()
-print("")
-Animal2.sleep()
-print("")
-Animal2.play()
-print("")
-Animal2.especies()
-print("")
 
 #------------ inheritence-------------------------------- 
 class Panda(Animal):
-    def __init__(self, nombre, especie, genero, edad, años_vida, origen):
+    def __init__(self, nombre, especie, genero, edad, life , origen,colores):
         super().__init__(nombre, especie, genero, edad)
-        self.años_vida = años_vida
+        self.life = life
         self.origen = origen
+        self.__colores = colores
 
     def information(self):
-        print(f"{self.nombre} es un {self.especie} el cual su tiempo de vida es de aproximadamente {self.años_vida}, según varias investigaciones se cree que su origen viene de {self.origen}")
+        return f"\n Nombre:{self.nombre}. \n Especie:{self.especie}. \n Tiempo de vida:{self.life}. \n Origen:{self.origen}."
+ #--------------------encapsulamiento----------------------
+    def get_colores(self):
+        """encapsulamiento de el color de piel de el oso"""
+        return f"\n Color de piel:{self.__colores}"
 
-
-Panda1 = Panda ("El Oso panda", " mamifero", "macho y hembra", 5,20, " La península iberica")
+Panda1 = Panda ("Oso panda", " mamifero", "macho y hembra", 5,"20 años", " La península iberica","blanco con lineas negras")
 
 print("EL OSO PANDA")
 print("")
 Panda1.information()
+print(Panda1.get_colores())
