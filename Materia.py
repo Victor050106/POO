ab31b2f2dbba5792ed57ba68d852e851b4275280
Victor <vimatoca050106@gmail.com>
2023-05-13 12:41:33 -0500

    refactor: se acomodó el codigo

--- Materia.py
+++ Materia.py
@@ -6,7 +6,9 @@ class Materia:
         self.salon = salon
 
     def informacion(self):
-        print(f"Materia:{self.nombre}\nImpartida por el profesor:{self.profesor}\nHorario:{self.horario}\nsalón:{self.salon}\n")
+        print(f"Materia:{self.nombre}\nImpartida por el profesor:{self.profesor}\n\
+              Horario:{self.horario}\n\
+              salón:{self.salon}\n")
 
 materia1 = Materia("Matemáticas", "Juan Pérez", "Lunes y Miércoles de 8:00 a 10:00", "A-101")
 materia2 = Materia("Historia", "María González", "Martes y Jueves de 10:00 a 12:00", "B-202")
@@ -18,10 +20,12 @@ class Calculo(Materia):
     def __init__(self, nombre, profesor, horario, salon,calificacion):
         super().__init__(nombre, profesor, horario, salon)
         self._calificacion =calificacion
-        
-    
+
     def informacion(self):
-        print(f"Materia:{self.nombre}\nImpartida por el profesor:{self.profesor}\nHorario:{self.horario}\nSalón:{self.salon}\n")
+        print(f"Materia:{self.nombre}\n\
+              Impartida por el profesor:{self.profesor}\n\
+              Horario:{self.horario}\n\
+              Salón:{self.salon}\n")
 
     def get_calificacion(self):
         return f"Calificación promedio:{self._calificacion}"
